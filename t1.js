ff291bf6bd282aafe5fc3beb2f853a1085bda28b
Neha14694 <duttagupta.neha@gmail.com>
2020-03-21 23:27:43 +0530

    first commit

---
+++ t1.js
@@ -0,0 +1,8 @@
+const g = { "Hello": [true, "Monkey", 4.2, { "hi": [] }], "Zoo": { "Monkey": "Amiya" }, "khalifa": true };
+
+console.log(g);
+// document.getElementById("b").innerHTML = g.Hello[1] + " is " + g.Zoo.Monkey;
+
+function test() {
+    document.getElementById("b").innerHTML = "My first JS";
+}
